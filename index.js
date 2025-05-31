ba0b998a51ed035edaa3006e0be465a81eb4414e
ayeshazahid296 <ayeshazahid296@gmail.com>
2025-05-31 10:35:41 +0500

    app.listen & get

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 //common js modules
-const express = require('express') //require is a function which calls or imports the modules of express
-const app = express() // ko call karke ek Express app banayi jo server banayegi
+const express = require('express'); // 'require' ek function hai jo Express module ko import karta hai
+const app = express(); // Express function ko call karke ek app banayi gayi jo server banayega
+
+// Server ko port 3000 par run kar rahe hain
+app.listen(3000, () => {
+    console.log("Server running at port 3000");
+});
+
+// Root route (homepage)
+app.get('/', (req, res) => {
+    res.send("Hello Everyone"); // res.send likhna zaroori hai
+});
+
+// Route for /name
+app.get('/name', (req, res) => {
+    res.send("My name is Ayesha"); // res.send missing tha
+});
+
+// Route for /uni
+app.get('/uni', (req, res) => {
+    res.send("I am studying in Bahria University"); // res.send missing tha
+});
+
+// Route for /hobby
+app.get('/hobby', (req, res) => {
+    res.send("My hobbies are painting and sketching"); // res.send missing tha
+});
+
+// Route for /friends
+app.get('/friends', (req, res) => {
+    res.send("I have 4 friends"); // res.send missing tha
+});
+
 
 const second = require("./second")
 console.log("Hello world", second)
