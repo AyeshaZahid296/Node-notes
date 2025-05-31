3443dcf59f399d5ce3ba02b8f2a82760c141e6ac
ayeshazahid296 <ayeshazahid296@gmail.com>
2025-05-31 09:59:24 +0500

    modified

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 //common js modules
+const express = require('express') //require is a function which calls or imports the modules of express
+const app = express() // ko call karke ek Express app banayi jo server banayegi
+
 const second = require("./second")
 console.log("Hello world", second)
