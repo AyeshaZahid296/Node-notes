ed77879ac77fd51a9221d2aa98733de102815c92
ayeshazahid296 <ayeshazahid296@gmail.com>
2025-06-01 10:25:21 +0500

    express.md

---
+++ Express.md
@@ -0,0 +1,25 @@
+# Express.JS Response Methods
+
+## res.send()
+send response to clint or browser ya text , html ,json ya ksi bhi data ko send kar sakta ha ... or responde ko end bhi kar data ha
+
+## res.json()
+This is the method of express which send data in javaScript object or json formate to clint... this is the standarad method to send responsse in apis
+
+## res.jsonp()
+This is a method of Express Js which send data in json padding form .... this is use for cross domain request network when browser cors does not allow 
+### cors 
+- cross origion resorse sharing 
+This is a security feature which allow browser decides that which domains can share data between them.
+
+## res.redirect()
+this is a express js method which is use to redirect to another rout the clint
+
+## res.render()
+## res.download()
+## res.sendFile()
+## res.end()
+## res.sendStatus()
+## res.headersSent()
+## res.set()
+## res.get()
--- index.js
+++ index.js
@@ -9,28 +9,15 @@ app.listen(3000, () => {
 
 // Root route (homepage)
 app.get('/', (req, res) => {
-    res.send("Hello Everyone");
+    res.send(`
+        Hello Everyone<br>
+        My name is Ayesha<br>
+        I am studying in Bahria University<br>
+        My hobbies are painting and sketching<br>
+        I have 4 friends
+    `);
 });
 
-// Route for /
-app.get('/', (req, res) => {
-    res.send("My name is Ayesha");
-});
-
-// Route for /
-app.get('/', (req, res) => {
-    res.send("I am studying in Bahria University");
-});
-
-// Route for /
-app.get('/', (req, res) => {
-    res.send("My hobbies are painting and sketching");
-});
-
-// Route for /
-app.get('/', (req, res) => {
-    res.send("I have 4 friends");
-});
 
 // Route for /name/user/:id (dynamic route)
 app.get('/name/user/:id', (req, res) => {
@@ -83,3 +70,58 @@ app.get('/admin/panel/overview', (req, res) => {
 // Importing another module
 const second = require("./second");
 console.log("Hello world", second);
+
+
+// Express JS Response Methods
+
+// ****** res.send() *******
+// these are the four ways to send response 
+app.get('/send', (req, res) => {  //  these all have same domain /send so thats why only the first one will run
+    res.send("Welcome to Home Page...")
+})
+
+app.get('/send', (req, res) => {
+    res.send(`<h1>Welcome to Home Page...</>`)
+})
+
+app.get('/send', (req, res) => {
+    res.send({ memeber: "Welcome to Home Page..." })
+})
+
+app.get('/send', (req, res) => {
+    res.send(["Ayesha", "Noor", "Laiba"])
+})
+
+// ****** res.json() *******
+
+app.get('/json', (req, res) => {
+    res.json({ message: "<h1>Welcome to JSON page...</h1>" });
+});
+
+// ****** res.jsonp() *******
+
+app.get('/jsonp', (req, res) => {
+    res.jsonp({ message: "<h1>Welcome to JSON page...</h1>" });
+});
+
+// ****** res.redirect() *******
+
+app.get('/home', (req, res) => { // is ma hum /home domain ma type kartay hi /welcome par redirsct ho  jain gay...
+    res.redirect('/welcome');
+});
+
+app.get('/welcome', (req, res) => {
+    res.send("<h1>Welcome to the new page to learn res.redirect!</h1>");
+});
+
+//redirect to an external website
+app.get('/google', (req, res) => { // is ma hum /google domain ma type kartay hi google par redirect ho jain gay..
+    res.redirect('https://www.google.com');
+});
+
+// ****** res.render() *******
+
+
+// ****** res.download() *******
+
+
